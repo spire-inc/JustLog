3b4c37300da2bdad7742d836b0ce5ab46d8eba9f
Ben Yule <bjyule@gmail.com>
2017-08-28 20:10:45 -0700

    Add Carthage Support

--- Gemfile
+++
@@ -1,6 +0,0 @@
-ruby '~> 2.4.0'
-
-source 'https://rubygems.org'
-
-gem 'cocoapods', '1.3.1'
-gem 'fastlane', '~> 2.63.0'
---
+++ JustLog/JustLog.h
@@ -0,0 +1,19 @@
+//
+//  JustLog.h
+//  JustLog
+//
+//  Created by Benjamin Yule on 8/28/17.
+//  Copyright © 2017 Spire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JustLog.
+FOUNDATION_EXPORT double JustLogVersionNumber;
+
+//! Project version string for JustLog.
+FOUNDATION_EXPORT const unsigned char JustLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JustLog/PublicHeader.h>
+
+
