daf79ff343292816d9213e600d3f85a92551ec9f
Justice Hsiung <Soulphalanx@gmail.com>
2018-06-06 00:19:37 -0700

    Update console log format

--- JustLog/Classes/Logger.swift
+++ JustLog/Classes/Logger.swift
@@ -71,7 +71,7 @@ public final class Logger: NSObject {
     }
     
     public func setup() {
-        let format = "$C $U $T:$N.$F $c | $M"
+        let format = "$C $U $X:$N.$F$c| $M"
         
         // console
         if enableConsoleLogging {
